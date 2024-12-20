852377cfcacd190c360d7565f8cc58b2e1c8f96a
Анатолий Сивков <a.sivkov@btc-s.ru>
2024-12-20 14:35:13 +0300

    fix linters

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from typing import List, Union
+from typing import List, Sequence, Union
 
 from fastapi import FastAPI, Path
 from fastapi.responses import JSONResponse
@@ -10,31 +10,27 @@ from database import engine
 app = FastAPI()
 
 
-@app.on_event("startup")
+@app.on_event('startup')
 async def startup():
     async with engine.begin() as conn:
         await conn.run_sync(models.Base.metadata.create_all)
 
 
-@app.post("/recipes", response_model=schemas.RecipeIn)
+@app.post('/recipes', response_model=schemas.RecipeIn)
 async def create(recipe: schemas.RecipeIn) -> JSONResponse:
     new_recipe = models.Recipe(**recipe.dict())
     await models.add_recipe(new_recipe=new_recipe)
-    return JSONResponse(content={"message": "Рецепт добавлен"}, status_code=201)
+    return JSONResponse(content={'message': 'Рецепт добавлен'}, status_code=201)
 
 
-@app.get("/recipes", response_model=List[schemas.RecipeListOut])
-async def recipes_list() -> List[models.Recipe]:
-    recipes = await models.get_recipes()
-    return recipes
+@app.get('/recipes', response_model=List[schemas.RecipeListOut])
+async def recipes_list() -> Sequence[models.Recipe]:
+    return await models.get_recipes()
 
 
-@app.get("/recipes/{idx}", response_model=schemas.RecipeDetailOut)
-async def detail(idx: int = Path(..., title="Идентификатор рецепта.", ge=0)) -> Union[models.Recipe, JSONResponse]:
+@app.get('/recipes/{idx}', response_model=schemas.RecipeDetailOut)
+async def detail(idx: int = Path(..., title='Идентификатор рецепта.', ge=0)) -> Union[models.Recipe, JSONResponse]:
     recipe = await models.get_recipe(idx=idx)
     if recipe:
         return recipe
-    return JSONResponse(content={"message": "Рецепт с указанным идентификатором не найден"}, status_code=404)
-
-
-# uvicorn main:app --reload
+    return JSONResponse(content={'message': 'Рецепт с указанным идентификатором не найден'}, status_code=404)
--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from typing import List, Union
+from typing import Sequence, Union
 
 from sqlalchemy import Column, Integer, String, select
 
@@ -6,14 +6,14 @@ from database import Base, async_session
 
 
 class Recipe(Base):
-    __tablename__ = "Recipe"
+    __tablename__ = 'Recipe'
 
-    id = Column(name="ID", type_=Integer, primary_key=True, index=True)
-    title = Column(name="Название", type_=String)
-    description = Column(name="Текстовое описание", type_=String)
-    ingredients = Column(name="Список ингредиентов", type_=String)
-    cooking_time = Column(name="Время готовки (в минутах)", type_=Integer, index=True)
-    views_count = Column(name="Количество просмотров", type_=Integer, index=True, default=0)
+    id = Column(name='ID', type_=Integer, primary_key=True, index=True)
+    title = Column(name='Название', type_=String)
+    description = Column(name='Текстовое описание', type_=String)
+    ingredients = Column(name='Список ингредиентов', type_=String)
+    cooking_time = Column(name='Время готовки (в минутах)', type_=Integer, index=True)
+    views_count = Column(name='Количество просмотров', type_=Integer, index=True, default=0)
 
 
 async def get_recipe(idx: int) -> Union[Recipe, None]:
@@ -23,13 +23,17 @@ async def get_recipe(idx: int) -> Union[Recipe, None]:
         if not (recipe := res.scalar_one_or_none()):
             return None
 
-        recipe.views_count += 1
+        if isinstance(recipe.views_count, int):
+            recipe.views_count += 1
+        else:
+            recipe.views_count = 1
+
         await session.commit()
 
         return recipe
 
 
-async def get_recipes() -> List[Recipe]:
+async def get_recipes() -> Sequence[Recipe]:
     async with async_session() as session:
         stmt = select(Recipe).order_by(Recipe.views_count.desc(), Recipe.cooking_time)
         res = await session.execute(stmt)
